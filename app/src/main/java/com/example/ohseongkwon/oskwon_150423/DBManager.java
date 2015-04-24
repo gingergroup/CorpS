94ac3a076e01eec836ef49632f26aebb11d5ea23
unknown <ohseongkwon@ohseongkwon.wsn.co.kr>
2015-04-24 18:01:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ohseongkwon/oskwon_150423/DBManager.java
@@ -0,0 +1,44 @@
+package com.example.ohseongkwon.oskwon_150423;
+/**
+ * Created by ohseongkwon on 2015-04-23.
+ */
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBManager extends SQLiteOpenHelper {
+    public DBManager(Context context, String name, CursorFactory factory, int version)
+    {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE FOOD_LIST( _id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price INTEGER);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    public void insert(String _query) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(_query);
+        db.close();
+    }
+
+    public void update(String _query) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(_query);
+        db.close();
+    }
+
+    public void delete(String _query) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(_query);
+        db.close();
+
+    }
+}
